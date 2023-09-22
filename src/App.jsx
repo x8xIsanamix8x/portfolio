f26a6eaa4a838179be43a14676f9de6120dea456
x8xIsanamix8x <x8xgoncalvesx8x@gmail.com>
2023-09-22 19:24:52 -0400

    First commit
    Little hobby, I will start a portfolio to present my projects.
    
    I have finished the banner and implemented a react-type-animation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Sidenav } from './components/Sidenav'
+import { Main } from './components/Main'
+
+export const App = () => {
+
+  return (
+    <div>
+      <Sidenav />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { TypeAnimation } from 'react-type-animation'
+import {FaTwitter, FaFacebook, FaInstagram, FaLinkedinIn } from 'react-icons/fa'
+
+import devbanner from '../assets/devbanner.jpg'
+
+
+export const Main = () => {
+  return (
+    <div id='main'>
+        <img 
+        className='object-cover' 
+        src={ devbanner } 
+        alt='/'
+        />
+        <div className='w-full h-screen object-cover absolute top-0'>
+            <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center'>
+                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800 text-white'>I'm José Goncalves</h1>
+                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800 text-white'>
+                    I'm a
+                    <TypeAnimation
+                        sequence={[
+                            'Developer',
+                            2000,
+                            'UX',
+                            2000,
+                            'Project Manager',
+                            2000,
+                        ]}  
+                        wrapper='div'
+                        cursor={true}
+                        repeat={Infinity}
+                        style={{ fontSize: '1em', paddingLeft: '5px' }}
+                    />
+                </h2>
+                <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+                    <FaTwitter className='cursor-pointer' size={20}/>
+                    <FaFacebook className='cursor-pointer' size={20}/>
+                    <FaInstagram className='cursor-pointer' size={20}/>
+                    <FaLinkedinIn className='cursor-pointer' size={20}/>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+
