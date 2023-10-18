483ae25f3001c72b46aa5d6b29f3389de29ce064
x8xIsanamix8x <x8xgoncalvesx8x@gmail.com>
2023-10-17 20:27:06 -0400

    Update V.2.0.3
    - Added finalized Work Experience editions.
    - Responsive Work Experience Section.
    
    Pending:
    - Add in the future the logos of the companies you work with.
    - Continue with the Completed Projects section.

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import  WorkItem  from './WorkItem'
+import homeborlogo from '../assets/homeborlogo.png'
 
 const data = [
     {
         year: 2019,
+        company: 'Homebor Platform',
         title: 'Project Manager | Software Engineer',
         duration: '4 Years',
         details: 
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid illum accusamus ratione molestias optio ab pariatur dolor ea facilis quod suscipit dolorem nisi atque, nesciunt veritatis, mollitia distinctio magni autem!'
+        'I was the first developer of the Homebor technology platform. Based on the requirements of its founder, I created infrastructure for the technological platform, organized the database, configured and managed the servers necessary for its use, and designed all the UX and UI to improve the user experience. I had the opportunity to find and organize my own team of programmers, and together with them I created a mobile application available in the Google Store and App Store. I also participated in meetings to achieve its sale. We managed to launch a specialized CRM for the management of international students in Canada, where any student in the world can register and manage their entire stay through the Homebor platform. I was the first developer of the Homebor technological platform. Based on the requirements of its founder, I created infrastructure for the technological platform, organized the database, configured and managed the servers necessary for its use, and designed all the UX and UI to improve the user experience. I had the opportunity to find and organize my own team of programmers, and together with them I created a mobile application available in the Google Store and App Store. I also participated in meetings to achieve its sale. We managed to launch a specialized CRM for the management of international students in Canada, where any student in the world can register and manage their entire stay through the Homebor platform.'
     },
     {
         year: 2015,
+        company: 'Centro Nacional de Tecnologías de Información',
         title: 'Project Manager | Software Engineer',
         duration: '4 Years',
         details: 
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid illum accusamus ratione molestias optio ab pariatur dolor ea facilis quod suscipit dolorem nisi atque, nesciunt veritatis, mollitia distinctio magni autem!'
+        'I participated as an intern in the Canaima GNU/Linux project. Due to my willingness and curiosity, I managed to stay permanently on the project. I went through all possible technical areas: technical support, server administrator, web developer and developer of the Canaima GNU distribution, until I became project leader. I organized meetings with different companies and government entities to obtain requirements and strategic alliances. My achievements in this period were launching three stable versions of the distribution, documenting the project for the first time and achieving great alliances with universities.'
     },
 
 ]
@@ -22,11 +25,12 @@ const data = [
 export const Work = () => {
   return (
     <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e] py-8'>Work Experience</h1>
         {data.map((item, idx) => (
             <WorkItem 
                 key={idx} 
                 year={item.year} 
+                company={item.company}
                 title={item.title} 
                 duration={item.duration} 
                 details={item.details}
--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const WorkItem = ({year, title, duration, details}) => {
+const WorkItem = ({year, company, title, image, duration, details}) => {
   return (
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
         <li className='mb-10 ml-4'>
@@ -9,12 +9,13 @@ const WorkItem = ({year, title, duration, details}) => {
                 <span className='inline-block px-2 py-1 font-semibold text-white bg-[#001b5e] rounded-md'>
                   {year}
                 </span>
-                <span className='text-lg font-semibold text-[#001b5e]'>{title}</span>
-                <span className='my-1 text-sm font-normal leading-none text-stone-400'>
-                  {duration}
-                </span>
+                <span className='text-lg font-semibold text-[#001b5e]'>{company}</span>
+                <span class="my-2 block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{title}</span>
             </p>
-            <p className='my-2 text-base font-normal text-stone-500'>{details}</p>
+            <span className='my-2 px-16 justify-content text-sm font-normal leading-none text-stone-400'>
+                  {duration}
+               </span>
+            <p className='my-2 px-16 text-justify font-normal text-stone-500'>{details}</p>
         </li>
     </ol>
   )
