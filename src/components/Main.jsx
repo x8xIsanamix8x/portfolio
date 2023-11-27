67946b3cacf4e367515f77916dc14aaedc67cdb8
x8xIsanamix8x <x8xgoncalvesx8x@gmail.com>
2023-11-27 13:54:22 -0400

    Update V.2.0.14
    Improved responsiveness on small devices and 1920x1080 in Main and Skills.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,13 +6,12 @@ import { TypeAnimation } from 'react-type-animation';
 import Profilecartoon from '../assets/Profilecartoon.jpg';
 import Descargas from '../assets/descargas.png';
 
-
 export const Main = () => {
+  const [nav, setNav] = useState(false);
 
-  const [nav, setNav ] = useState(false);
-    const handleNav = () => {
-        setNav(!nav);
-    };
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
     <div id='main' className="relative w-screen min-h-screen h-screen flex flex-col lg:flex-row">
@@ -24,14 +23,14 @@ export const Main = () => {
         />
       </div>
 
-      <div className="w-full lg:w-1/2 h-1/2 lg:h-full sm:p-8 flex flex-col justify-center items-start">
-        <h1 className="animate__animated animate__fadeInDown text-4xl text-3x1 font-bold text-gray-800 mt-2 sm:mt-4 lg:mt-0 text-start">
+      <div className="w-full lg:w-1/2 h-1/2 lg:h-full sm:p-8 flex flex-col justify-center items-center sm:items-start">
+        <h1 className="animate__animated animate__fadeInDown text-4xl text-3xl font-bold text-gray-800 mt-2 sm:mt-4 lg:mt-0 text-center sm:text-left">
           Hi, my name is José Goncalves,
         </h1>
 
-        <h2 className='flex text-3xl text-2x1 pt-2 sm:pt-4 animate__animated animate__fadeInLeft'>
+        <h2 className='flex text-3xl text-2xl pt-2 sm:pt-4 animate__animated animate__fadeInLeft text-center sm:text-left'>
           I'm a
-          <TypeAnimation 
+          <TypeAnimation
             sequence={['FullStack', 2000, 'UX', 2000, 'Project Manager', 2000]}
             wrapper='div'
             repeat={Infinity}
@@ -39,17 +38,18 @@ export const Main = () => {
             style={{ fontSize: '0.9em', paddingLeft: '5px' }}
           />
         </h2>
-        <p className="flex mt-1 mb-8 text-lg sm:mb-12 pt-4 items-justify text-start animate__animated animate__fadeInRight">
-        I'm delighted to welcome you to my portfolio. I'm a passionate Full Stack web developer witg a diverse range of skills and industry experience.
+        <p className="flex mt-1 mb-8 text-lg sm:mb-12 pt-4 items-justify text-center sm:text-left animate__animated animate__fadeInRight">
+          I'm delighted to welcome you to my portfolio. I'm a passionate Full Stack web developer with a diverse range of skills and industry experience.
         </p>
 
         <div className='flex items-center sm:flex-row justify-center justify-between space-x-4 sm:space-x-6'>
-          <a href="https://drive.google.com/file/d/11bsil4kYlGysLUDW_QqjUTtl1xDZASqK/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='flex bg-blue-700 text-white w-full sm:w-auto px-6 py-3 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-700'>
+          <a href="https://drive.google.com/file/d/11bsil4kYlGysLUDW_QqjUTtl1xDZASqK/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='flex bg-blue-700 text-white w-full sm:w-auto px-6 py-3 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-700 items-center
+justify-center'>
             Resume
             <img className="ml-2 mt-1 animate-bounce" src={Descargas} alt="Descargas Icon" />
           </a>
-          <a className='text-black outline outline-[#040404] w-full sm:w-auto px-6 py-3 rounded-lg'
-          onClick={ handleNav } href="#contact"
+          <a className='items-center justify-center text-black outline outline-[#040404] w-full sm:w-auto px-6 py-3 rounded-lg'
+            onClick={handleNav} href="#contact"
           >
             Contact
           </a>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,10 +4,6 @@ import {
     FaCss3Alt,
     FaBootstrap,
     FaReact,
-    FaTwitter,
-    FaFacebook,
-    FaInstagram,
-    FaLinkedinIn,
   } from 'react-icons/fa';
   import { AiOutlineHtml5 } from 'react-icons/ai';
   import { BiLogoTailwindCss, BiLogoPhp, BiLogoGraphql, BiLogoMongodb, BiLogoFirebase } from 'react-icons/bi';
@@ -43,33 +39,35 @@ export const Skills = () => {
 
   return (
 
-    <div id='about' className='max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between items-center xl:items-start gap-12 w-full py-16 px-12'>
-      <div className='flex-1 flex flex-col gap-4 '>
+    <div id='about' className='max-w-screen-2xl flex flex-col xl:flex-row xl:justify-center items-center xl:items-start gap-12 w-full py-16 px-8 sm:px-12'>
 
-        <div className='mx-5 mb-8'>
-          <h1 className='text-4xl font-bold text-start text-[#001b5e] inline border-b-4 border-[#001b5e]'>About me</h1>
+      <div className='flex-1 flex flex-col gap-4 text-center xl:text-left'>
+
+        <div className='mx-2 sm:mx-5'>
+          <h1 className='text-4xl font-bold text-[#001b5e] inline border-b-4 border-[#001b5e]'>About me</h1>
         </div>
 
-        <p className='flex mt-1 mb-8 mx-5 text-lg sm:mb-12 pt-4 items-justify text-justify'>
+        <p className='flex mt-1 mb-8 mx-2 sm:mx-5 text-lg sm:mb-12 pt-4 items-center text-justify'>
           I'm a creative engineer, dedicated to crafting engaging and user-friendly experiences. With
-          several years of a cereer in the web development world, I've had the privilege to work across various areas,
+          several years of a career in the web development world, I've had the privilege to work across various areas,
           from UI design to the implementation of robust technical solutions.
         </p>
 
       </div>
 
-      <div className='grid mx-5 grid-cols-3 grid-rows sm:grid-cols-5 lg:grid-cols gap-1 lg:mt-20 auto-rows-max'>
-             {skills.map((skill, index) => (
-               <a key={index} className="w-full bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                 {skill.icon}
-                 <div class="text-left">
-                   <div class="ml-1 font-sans text-sm font-semibold">{skill.text}</div>
-                 </div>
-               </a>
-             ))}
-           </div>
+      <div className='grid mx-2 sm:mx-5 grid-cols-3 grid-rows sm:grid-cols-5 lg:grid-cols gap-1 lg:mt-20 auto-rows-max'>
+        {skills.map((skill, index) => (
+          <a key={index} className="w-full bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+            {skill.icon}
+            <div className="text-left">
+              <div className="ml-1 font-sans text-sm font-semibold">{skill.text}</div>
+            </div>
+          </a>
+        ))}
+      </div>
 
     </div>
+
 
 
 
